5c7184a3eb4a77239a5f11b13ec1a9a8ff2205a2
Uzairak01 <h.uzairak01@gmail.com>
2024-09-04 00:12:54 +0500

    fixed errors

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function secondsToMinutesSeconds(seconds) {
 async function getSongs(folder) {
     
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`http://127.0.0.1:5501/${folder}/`)
     let response = await a.text();
     
     let div = document.createElement("div")
